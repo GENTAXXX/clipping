8476d0e1c32ac64f3d088c64c33355fc77fda9f1
GENTAXXX <bramantyaraka00@mail.ugm.ac.id>
2020-07-23 10:11:14 +0700

    Add Role, Project

--- app/News.php
+++ app/News.php
@@ -9,7 +9,7 @@ class News extends Model
     protected $primaryKey = 'news_id';
     protected $fillable = [
         'news_id','news_title','news_desc','news_extract','news_status','news_area','news_approval','news_approval_date','news_created',
-        'media_id','news_date','categories','keywords','lang_id','verificator_id','creator_id','image'
+        'media_id','news_date','categories','keywords','lang_id','verificator_id','creator_id','project_id','image'
     ];
 
     public function language(){
